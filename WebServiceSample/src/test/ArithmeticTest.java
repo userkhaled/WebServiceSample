61742aa3978035f340da8a76d49e183e558095ec
abd <abd@DESKTOP-CM91TFJ>
2020-12-16 21:31:36 +0200

    first commit

---
+++ WebServiceSample/src/test/ArithmeticTest.java
@@ -0,0 +1,13 @@
+
+package test;
+
+public class ArithmeticTest {
+	public float addValues(float a, float b) {
+		return (a + b);
+	}
+ 
+	public float subtractValues(float a, float b) {
+		return (a - b);
+	}
+
+}
